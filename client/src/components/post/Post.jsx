67b265535955ba72da1ecfb3989279f2508643e2
Anthony Malary <amalary@bu.edu>
2022-06-16 13:31:55 -0700

    Made progress on app have to fix image issue

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -2,16 +2,15 @@ import "../post/post.css"
 import {MoreVert} from "@mui/icons-material"
 // import { Users } from "../../exampleData"
 import { useEffect,useState} from 'react'
-import axios from "axios"
+import axios from "axios" 
 
 export default function Post({post}) {
     
-    const [likes,setLike] = useState(post.likes)
+    const [likes,setLike] = useState(post.likes.length)
     const [isLiked,setIsLiked] = useState(false)
     const [user,setUser] = useState({})
 
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-
 
     useEffect(()=> {
 
@@ -22,7 +21,10 @@ export default function Post({post}) {
     
         }
         fetchUser()
-      },[])
+},[])
+
+
+
 
     const likeHandler =()=> {
         setLike(isLiked ? likes-1 : likes+1)
@@ -36,8 +38,8 @@ export default function Post({post}) {
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopleft">
-                        <img className="postProfileImg" src= {user.profilePicture} alt="" />
-                        <span className="postUserName">{user.userName}</span>
+                        <img className="postProfileImg" src= {user.profilePicture || PF + "profilePictures/noCover.png"} alt="" />
+                        <span className="postUserName">{post.userName}</span>
                         <span className="postDate">{post.date}</span>
                     </div>
                     <div className="postTopRight"></div>
@@ -45,16 +47,16 @@ export default function Post({post}) {
                 </div>
                 <div className="postCenter">
                     <span className="postText">{post?.desc}</span>
-                    <img className = "postImg" src= {PF+post.photo} alt="Selfie of spiderman on top of empire state building" />
+                    <img className = "postImg" src= {PF+post.img} alt="" />
                 </div>
                 <div className="postBottom">
                     <div className="postBottomLeft">
                         <img className = "likeIcon" src={`${PF}Icon/likeButton.png`} onClick={likeHandler} alt="" />
                         <img className = "likeIcon" src={`${PF}Icon/heartButton.jpg`} onClick={likeHandler} alt="" />
-                        <span className="postLikeCounter">{likes}</span>
+                        <span className="postLikeCounter">{likes} person likes this</span>
                     </div>
                     <div className="postBottomRight">
-                        <span className="postCommentText">{post.comments}</span>
+                        <span className="postCommentText"> comments{post.comments}</span>
                     </div>
                 </div>
             </div>
